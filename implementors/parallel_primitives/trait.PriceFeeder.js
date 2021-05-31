e12268b89ed7a383533a9ee78407ac3b6637c2e7
GopherJ <GopherJ@users.noreply.github.com>
2021-05-31 13:45:34 +0000

    deploy: a223cd7910af3540048b58f958fea5b784876468

---
+++ implementors/parallel_primitives/trait.PriceFeeder.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pallet_prices"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_prices/module/trait.Config.html\" title=\"trait pallet_prices::module::Config\">Config</a>&gt; <a class=\"trait\" href=\"parallel_primitives/trait.PriceFeeder.html\" title=\"trait parallel_primitives::PriceFeeder\">PriceFeeder</a> for <a class=\"struct\" href=\"pallet_prices/module/struct.Pallet.html\" title=\"struct pallet_prices::module::Pallet\">Pallet</a>&lt;T&gt;","synthetic":false,"types":["pallet_prices::module::Pallet"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/parity_scale_codec/compact/trait.CompactAs.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["parallel_primitives"] = [{"text":"impl CompactAs for <a class=\"struct\" href=\"parallel_primitives/struct.PriceWithDecimal.html\" title=\"struct parallel_primitives::PriceWithDecimal\">PriceWithDecimal</a>","synthetic":false,"types":["parallel_primitives::PriceWithDecimal"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
